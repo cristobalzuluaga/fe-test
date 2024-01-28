422aeb50757a13477067d6bb1319422cb81551d9
cristobalzuluaga <96295130+cristobalzuluaga@users.noreply.github.com>
2024-01-28 08:59:09 -0500

    Feature/environments (#14)
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow
    
    * changes in workflow

--- terraform/00-variables.tf
+++ terraform/00-variables.tf
@@ -1,5 +1,5 @@
-variable "branch_ref" {
-  description = "branch reference that contains 'refs/heads/branch'"
-  default     = "refs/heads/dev"
+variable "branch_name" {
+  description = "branch name of the build"
+  default     = "dev"
 }
 
